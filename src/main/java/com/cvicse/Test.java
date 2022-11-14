5141b20f57d708d5f7f7ff36ddfb9768e6038bdd
SSK <1796727601@qq.com>
2022-11-14 17:22:11 +0800

    Initial commit

---
+++ src/main/java/com/cvicse/Test.java
@@ -0,0 +1,14 @@
+package com.cvicse;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("ASDAS");
+        System.out.printf("ASDAS");
+
+        System.out.printf("ASDAS");
+
+        System.out.printf("ASDAS");
+
+
+    }
+}
