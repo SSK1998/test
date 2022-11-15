aab453c8f508407198c08d7e91fde023c3ea29dc
SSK <1796727601@qq.com>
2022-11-15 14:42:32 +0800

    f -->ln

--- src/main/java/com/cvicse/Test.java
+++ src/main/java/com/cvicse/Test.java
@@ -2,12 +2,12 @@ package com.cvicse;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.printf("ASDAS");
-        System.out.printf("ASDAS");
+        System.out.println("ASDAS");
+        System.out.println("ASDAS");
 
-        System.out.printf("ASDAS");
+        System.out.println("ASDAS");
 
-        System.out.printf("ASDAS");
+        System.out.println("ASDAS");
 
 
     }
